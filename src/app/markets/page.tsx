890544e3eae3ed8262ff7fd3e096ef32908bb6a8
Mac <mac@Macs-MacBook-Air.local>
2025-04-27 10:12:56 -0700

    fix build

--- src/app/markets/page.tsx
+++ src/app/markets/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div className="m-auto h-[50vh] flex justify-center items-center">
         <div className="">
-            <p className="text-3xl font-bold m-auto">Markets in progress</p>
+            <p className="text-3xl font-bold m-auto">Markets in progress...</p>
         </div>
     </div>
   )
