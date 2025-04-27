0ba51dbd9b140564e34c4ddb258167755eb59465
Mac <mac@Macs-MacBook-Air.local>
2025-04-27 08:22:00 -0700

    add pages

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,6 +2,10 @@
 
 export default function page() {
   return (
-    <div>portfolio in progress</div>
+    <div className="m-auto h-[50vh] flex justify-center items-center">
+        <div className="">
+            <p className="text-3xl font-bold m-auto">portfolio in progress</p>
+        </div>
+    </div>
   )
 }
